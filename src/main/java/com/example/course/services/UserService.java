26a24c0ad0fced2774daa06c03b9e0399a32b84b
marcb <marcbitenl@gmail.com>
2022-02-14 19:23:43 -0300

    User update

--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -32,6 +32,21 @@ public class UserService {
 	public void delete (Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getById(id);
+		udpadeteData(entity, obj);
+		return repository.save(entity);
+		
+	}
+
+
+	private void udpadeteData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
+	}
 }
 
 
